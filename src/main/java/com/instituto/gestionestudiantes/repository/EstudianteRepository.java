222669932dc348168c2ac1895208d5e98a72c867
ConcepcionArequipa <concepcion.arequipa@epn.edu.ec>
2026-01-15 01:15:12 -0500

    Se finalizo la logica

--- src/main/java/com/instituto/gestionestudiantes/repository/EstudianteRepository.java
+++ src/main/java/com/instituto/gestionestudiantes/repository/EstudianteRepository.java
@@ -1,4 +1,15 @@
 package com.instituto.gestionestudiantes.repository;
 
-public class EstudianteRepository {
+import com.instituto.gestionestudiantes.entity.Estudiante;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+// Spring Data JPA crea automáticamente métodos CRUD básicos del documento
+
+@Repository
+public interface EstudianteRepository extends JpaRepository<Estudiante, Long> {
+
 }
