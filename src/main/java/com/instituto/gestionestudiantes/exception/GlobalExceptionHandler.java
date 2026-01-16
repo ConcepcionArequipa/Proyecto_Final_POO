8e14521abc8b4f66ce0dc9dda16717432158e3a3
ConcepcionArequipa <concepcion.arequipa@epn.edu.ec>
2026-01-16 01:55:16 -0500

    Se realizo la clase con @ControllerAdvice para manejo global de excepciones

---
+++ src/main/java/com/instituto/gestionestudiantes/exception/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.instituto.gestionestudiantes.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    // Para recursos no encontrados 404
+    @ExceptionHandler(RecursoNoEncontradoException.class)
+    public ResponseEntity<Object>manejarRecursoNoEncontrado(RecursoNoEncontradoException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status",404);
+        body.put("error","Not found" );
+        body.put("message",ex.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+
+    // La solicitud contiene errores o datos inválidos (400)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object>manejarMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status",400);
+        body.put("error","Bad Request" );
+        body.put("message","Error de validacion");
+
+    }
+}
