931a618bee52b8376538a7efef78ecd2d611142e
jossuer3 <rojasjosue55a@gmail.com>
2026-01-18 18:45:09 -0500

    se agrega swagger

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
 INSERT INTO estudiantes (nombre, apellido, cedula, email, edad, carrera, semestre) VALUES
-('JUAN', 'PEREZ', '0102030401', 'juan.perez@mail.com', 20, 'INGENIERIA DE SISTEMAS', 3),
-('MARIA', 'LOPEZ', '0102030402', 'maria.lopez@mail.com', 22, 'INGENIERIA EN SOFTWARE', 5),
-('CARLOS', 'GOMEZ', '0102030403', 'carlos.gomez@mail.com', 19, 'INGENIERIA ELECTRICA', 2),
-('ANA', 'TORRES', '0102030404', 'ana.torres@mail.com', 21, 'INGENIERIA MECANICA', 4),
-('LUIS', 'MORA', '0102030405', 'luis.mora@mail.com', 23, 'INGENIERIA CIVIL', 6),
-('DIANA', 'RIVAS', '0102030406', 'diana.rivas@mail.com', 18, 'TECNOLOGIA SUPERIOS EN DESARROLLO DE SOFTWARE', 1),
-('PEDRO', 'CASTRO', '0102030407', 'pedro.castro@mail.com', 24, 'INGENIERIA ELECTRONICA Y TELECOMUNICACIONES', 7),
-('SOFIA', 'VEGA', '0102030408', 'sofia.vega@mail.com', 20, 'INGENIERIA DE SISTEMAS', 3),
-('MIGUEL', 'SALAZAR', '0102030409', 'miguel.salazar@mail.com', 22, 'INGENIERIA INDUSTRIAL', 5),
-('LAURA', 'NAVARRO', '0102030410', 'laura.navarro@mail.com', 19, 'INGENIERIA AMBIENTAL', 2);
+('JUAN', 'PEREZ', '1102030401', 'juan.perez@gmail.com', 20, 'INGENIERIA DE SISTEMAS', 3),
+('MARIA', 'LOPEZ', '0702030402', 'maria.lopez@gmail.com', 22, 'INGENIERIA EN SOFTWARE', 5),
+('CARLOS', 'GOMEZ', '1502030403', 'carlos.gomez@gmail.com', 19, 'INGENIERIA ELECTRICA', 2),
+('ANA', 'TORRES', '1202030404', 'ana.torres@gmail.com', 21, 'INGENIERIA MECANICA', 4),
+('LUIS', 'MORA', '1702030405', 'luis.mora@gmail.com', 23, 'INGENIERIA CIVIL', 6),
+('DIANA', 'RIVAS', '1002030406', 'diana.rivas@gmail.com', 18, 'TECNOLOGIA SUPERIOS EN DESARROLLO DE SOFTWARE', 1),
+('PEDRO', 'CASTRO', '0902030407', 'pedro.castro@gmail.com', 24, 'INGENIERIA ELECTRONICA Y TELECOMUNICACIONES', 7),
+('SOFIA', 'VEGA', '0402030408', 'sofia.vega@gmail.com', 20, 'INGENIERIA DE SISTEMAS', 3),
+('MIGUEL', 'SALAZAR', '0302030409', 'miguel.salazar@gmail.com', 22, 'INGENIERIA INDUSTRIAL', 5),
+('LAURA', 'NAVARRO', '0502030410', 'laura.navarro@gmail.com', 19, 'INGENIERIA AMBIENTAL', 2);
