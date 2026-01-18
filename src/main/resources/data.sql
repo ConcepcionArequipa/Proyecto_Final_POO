aeb37d22448a3497491370c2440d1f8233893a2c
jossuer3 <rojasjosue55a@gmail.com>
2026-01-18 15:15:22 -0500

    DATOS PRECARGADOS EN datos.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO estudiantes (nombre, apellido, cedula, email, edad, carrera, semestre) VALUES
+('JUAN', 'PEREZ', '0102030401', 'juan.perez@mail.com', 20, 'INGENIERIA DE SISTEMAS', 3),
+('MARIA', 'LOPEZ', '0102030402', 'maria.lopez@mail.com', 22, 'INGENIERIA EN SOFTWARE', 5),
+('CARLOS', 'GOMEZ', '0102030403', 'carlos.gomez@mail.com', 19, 'INGENIERIA ELECTRICA', 2),
+('ANA', 'TORRES', '0102030404', 'ana.torres@mail.com', 21, 'INGENIERIA MECANICA', 4),
+('LUIS', 'MORA', '0102030405', 'luis.mora@mail.com', 23, 'INGENIERIA CIVIL', 6),
+('DIANA', 'RIVAS', '0102030406', 'diana.rivas@mail.com', 18, 'TECNOLOGIA SUPERIOS EN DESARROLLO DE SOFTWARE', 1),
+('PEDRO', 'CASTRO', '0102030407', 'pedro.castro@mail.com', 24, 'INGENIERIA ELECTRONICA Y TELECOMUNICACIONES', 7),
+('SOFIA', 'VEGA', '0102030408', 'sofia.vega@mail.com', 20, 'INGENIERIA DE SISTEMAS', 3),
+('MIGUEL', 'SALAZAR', '0102030409', 'miguel.salazar@mail.com', 22, 'INGENIERIA INDUSTRIAL', 5),
+('LAURA', 'NAVARRO', '0102030410', 'laura.navarro@mail.com', 19, 'INGENIERIA AMBIENTAL', 2);
